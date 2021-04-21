1959bfbe02cd0cbf77483e2146e322b0ff8532b1
Jacob Aguilar <aguilar.campos.jacob@gmail.com>
2021-04-21 18:06:51 -0400

    Initial Commit

---
+++ Shared/BasicContactAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  BasicContactAppApp.swift
+//  Shared
+//
+//  Created by Jacob Aguilar on 21-04-21.
+//
+
+import SwiftUI
+
+@main
+struct BasicContactAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
